bf03046a7cd767b6dbd6594c651ebf0d3ef74eb8
Seunggu Kim <sgkim08@gmail.com>
2019-08-19 22:40:39 +0900

    web python basics #1

---
+++ 06_if_cond/05_if_cond.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+print("Content-Type: text/html")
+print()
+print('Hello web')
+
+import cgi
+form = cgi.FieldStorage()
+pageId = form["id"].value
+
+#	docstring
+print('''<!doctype html>
+<html>
+<head>
+	<title>WEB - Welcome</title>
+	<meta charset="utf-8">
+</head>
+<body>
+	<h1><a href="04_url_query_string.py">WEB</a></h1>
+	<ol>
+		<li><a href="04_url_query_string.py?id=HTML">HTML</a></li>
+		<li><a href="04_url_query_string.py?id=CSS">CSS</a></li>
+		<li><a href="04_url_query_string.py?id=JavaScript">JavaScript</a></li>
+	</ol>
+	<h2>{title}</h2>
+	<p>The World Wide Web (abbrebiated WWW or the Web) is ...
+	</p>
+</body>
+</html>
+'''.format(title=pageId))
+
